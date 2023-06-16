cc21b7a8f0627383d35fd103ee600fde65836638
AaronHtlDornbirn <91245441+AaronHtlDornbirn@users.noreply.github.com>
2023-06-16 14:28:53 +0200

    Header erstellt

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,11 +1,25 @@
 function Header(){
     return (
     <div className="">
-        <div className="h-screen w-full bg-cyan-800 text-white">
-            hellou
-        </div>
-        <div className="h-screen w-full bg-cyan-500 text-white">
-            hellou
+        <div className="bg-blue-300 p-5 border-b-2 border-black">
+            <h1 className="text-white text-center text-4xl font-bold">Calendar</h1>
+            <div className="flex items-center justify-left space-x-4">
+                <button className="bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded">
+                {'<<'}
+                </button>
+                <button className="bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded">
+                {'<'}
+                </button>
+                <div className="bg-white text-black border-2 border-black font-bold py-2 px-4 rounded">
+                    Testmonat
+                </div>
+                <button className="bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded">
+                {'>'}
+                </button>
+                <button className="bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded">
+                {'>>'}
+                </button>
+            </div>
         </div>
     </div>
     );        
