3d70bce65c9575fecad9cc56dc7ada8f903f7e00
KGahan <kimgahan05@gmail.com>
2023-06-16 12:37:18 +0200

    first commit, tried something

---
+++ src/Components/Header.tsx
@@ -0,0 +1,14 @@
+function Header(){
+    return (
+    <div className="">
+        <div className="h-screen w-full bg-cyan-800 text-white">
+            hellou
+        </div>
+        <div className="h-screen w-full bg-cyan-500 text-white">
+            hellou
+        </div>
+    </div>
+    );        
+}
+
+export default Header;
