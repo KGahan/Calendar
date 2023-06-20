1b875f0e9c3e9060eb860a10b2ac7e12a8d1edde
AaronHtlDornbirn <91245441+AaronHtlDornbirn@users.noreply.github.com>
2023-06-20 09:06:46 +0200

    PopUp verbessert

--- src/Components/PopUp.tsx
+++ src/Components/PopUp.tsx
@@ -2,28 +2,26 @@ function PopUp() {
 
     return (
         <div className="absolute h-full w-full flex justify-center items-center bg-black bg-opacity-50">
-            <div className="h-2/3 w-1/3 bg-white border-2 border-black">
+            <div className="w-1/3 bg-white border-2 border-black">
                 <div className="w-full p-5 bg-blue-300 text-white text-center text-2xl font-bold border-b-2 border-black">Add an Event</div>
                 <div className="p-5">
                     <input
-                        className="bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded"
+                        className="mb-5 w-full bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded"
                         type="text"
                         placeholder="Title"
                     />
-                </div>
-                <div className="pl-5 pb-5 pr-5">
-                    <textarea                      
-                        className="w-full bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded"
+                    <textarea
+                        className="mb-5 w-full bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded"
                         placeholder="Description"
                     ></textarea>
-                </div>
-                <div className="pl-5 pb-5 pr-5 flex justify-between">
-                    <button className="bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded">
-                        Exit
-                    </button>
-                    <button className="bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded">
-                        Finish
-                    </button>
+                    <div className="flex justify-end">
+                        <button className="bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 mr-2 rounded-md">
+                            Exit
+                        </button>
+                        <button className="bg-white text-black hover:bg-blue-200 border-2 border-black font-bold py-2 px-4 rounded-md">
+                            Finish
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
